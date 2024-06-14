5d305279a1a526989053bcf0e21b54a26dac4378
capnmav77 <its.mastermind77@gmail.com>
2024-06-14 13:15:29 +0530

    handled edge case

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ source_x, source_y, source_z = 0, 0, 0
 dest_x, dest_y, dest_z = 9, 9, 4
 
 partitioned_map = MapPartitioner.read_map_from_file("map.txt")
+if(partitioned_map == None):
+    print("Error reading map from file")
+    exit()
 path = dijkstra(partitioned_map, source_x, source_y, source_z, dest_x, dest_y, dest_z)
 if path:
     print("Shortest path:")
